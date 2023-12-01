c5f7d1fbff5465c66c27162f21abb97258a2e686
Andrew Winter <andrew@MacBook-Pro-6.local>
2023-12-01 14:44:40 +0100

    updated common definitions

--- general/common.py
+++ general/common.py
@@ -2,8 +2,6 @@ import os
 import sys
 import numpy as np
 
-NBODYDIR = '/usr/local/bin/'
-NPROCS=8
 
 scriptdir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(scriptdir)
@@ -17,6 +15,8 @@ photoevapdir = rootdir+'/photoevap'
 nbodydir = rootdir+'/nbody'
 plotdir = rootdir+'/plot'
 
+
+
 sys.path.append(rootdir)
 sys.path.append(photoevapdir)
 sys.path.append(nbodydir)
@@ -28,11 +28,28 @@ import pyximport; pyximport.install(setup_args={'include_dirs':[np.get_include()
 #import cluster_calcs as cc
 sys.path.append(scriptdir+'/nbody')
 
+
+exepath = 'nbody_path.txt'
+NBODYEXE=None
+if os.path.isfile(exepath):
+    with open(exepath) as f:
+        lines = f.readlines()
+        for line in lines:
+            if len(line)>2:
+                NBODYEXE = line
+                break
+if NBODYEXE is None:
+    print('No file (%s) with location of Nbody6++ found...'%exepath)
+    NBODYEXE = '/usr/local/bin/nbody6++'
+    print('Assuming: %s'%NBODYEXE)
+else:
+    print('Nbody6++ executable location:', NBODYEXE)
+
+NPROCS=8
 SYSPATH =sys.path
 
 
 G_si = 6.67e-11
-
 s2myr = 3.17098e-8*1e-6
 m2au = 6.68459e-12
 m2pc = 3.24078e-17
