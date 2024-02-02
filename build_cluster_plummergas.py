6acbb931c9b048742f380ec3aef2e36aeba747aa
Andrew Winter <andrew@MacBook-Pro-6.local>
2024-02-02 11:23:47 +0100

    Removed erroneous keyword

--- build_cluster_plummergas.py
+++ build_cluster_plummergas.py
@@ -230,7 +230,7 @@ if __name__=='__main__':
             sim = nbi.nbody6_cluster(rs.T, vs.T, ms,  outname='clustersim', dtsnap_Myr =0.2, \
                         tend_Myr = tend, gasparams=gparams, etai=0.005, etar=0.005, etau=0.01, dtmin_Myr=1e-8, \
                         rmin_pc=1e-5,dtjacc_Myr=1.0, load=True, ctype='smooth', force_incomp = False, \
-                        rtrunc=50.0, nbin0=nbins0, aclose_au=200.0, tide='none')
+                        rtrunc=50.0, nbin0=nbins0, aclose_au=200.0)
             #sim.store_arrays(reread=True)
 
             sim.evolve(reread=False, suppress_restart=False)
