45344974e03681ae9c603d4da06219996d188a97
Andrew WINTER <Andrew Winter>
2024-04-01 19:58:25 +0200

    edited plummer_gas

--- build_cluster_plummergas.py
+++ build_cluster_plummergas.py
@@ -36,6 +36,7 @@ mas2rad = 4.848e-9
 
 day2s = 60.*60.*24.0
 year2s = day2s*365.
+Myr2s = year2s*1e6
 pc2cm = 3.086e18
 Gcgs = 6.6743e-8
 Msol2g = 1.988e33
@@ -149,153 +150,159 @@ if __name__=='__main__':
     
 
 
-    tend = 100.0
+	tend = 50.0 #100.0
 
-    # Parameters
-    a = 5.0    # Scale radius
-    M = 1e5    # Total mass
+	# Parameters
+	a = 3.0    # Scale radius
+	M = 1e5    # Total mass
 
-    texp = 0.0
-    trem=20.0
-    alphas = np.array([0.1, 0.25, 0.5])
-    betas = np.array([0.1, 0.25, 0.5])
-    num_rows = len(alphas)
-    num_cols = len(betas)
-
-    figsize = (12, 12)  # Adjust the figure size as needed
-
-    # Create the figure and gridspec
-    fig1 = plt.figure(figsize=figsize)
-    fig2 = plt.figure(figsize=figsize)
-    fig3 = plt.figure(figsize=figsize)
-    gs1 = gridspec.GridSpec(num_rows, num_cols, figure=fig1, hspace=0, wspace=0)
-    gs2 = gridspec.GridSpec(num_rows, num_cols, figure=fig2, hspace=0, wspace=0)
-    gs3 = gridspec.GridSpec(num_rows, num_cols, figure=fig2, hspace=0, wspace=0)
-
-    homdir = os.getcwd()
-    for irow, alpha in enumerate(alphas):
-        for icol, beta in enumerate(betas):
-            print('Current working directory:', os.getcwd())
-            dname = 'alpha_%.2lf_beta_%.2lf'%(alpha, beta)
-            if not os.path.isdir(dname):
-                print('No directory %s found, creating...'%dname)
-                os.makedirs(dname)
-            os.chdir(dname)
-            if os.path.isfile(homdir+'/nbody_path.txt'):
-                print('Found nbody_path, copying to current directory')
-                shutil.copy(homdir+'/nbody_path.txt', './nbody_path.txt')
-            else:
-                print('No nbody_path found, assuming default is correct...')
+	#texp = 2.0
+	trem=0.0
+	alphas = np.array([0.1, 0.2])
+	betas = np.array([0.2, 0.5])
+	gammas = np.array([0.2, 1.0, 5.0])
+	num_rows = len(alphas)
+	num_cols = len(betas)
 
 
-            if not os.path.isfile('sim_ics_r.npy') or not os.path.isfile('sim_ics_v.npy') or not os.path.isfile('sim_ics_m.npy') or not os.path.isfile('sim_gparams.npy'):
-                
-                Mgas = alpha*M
-                agas = beta*a
+	figsize = (6, 6)  
 
-                #gparams = define_gasjumps(Mgas, -Mgas/trem, njumps=1, tdelay=1.0, tend=tend, ascale=1.0)
-                if texp==0.0:
-                	gparams = np.array([Mgas, agas,0.0, trem])
-                else:
-                	gparams = np.array([Mgas, agas,Mgas/texp, trem])
-                	
-                mmin= 0.08
+	homdir = os.getcwd()
+	for ig, gamma in enumerate(gammas):
 
-                imf_func = bc.get_kroupa_imf(mmin=mmin)
-                msp = np.logspace(-2, 2, 1000)
-                mmass = np.trapz(msp*imf_func(msp), msp)/np.trapz(imf_func(msp), msp)
-                
-                mmass = 1.
-                N = int(M/mmass)
+		# Create the figure and gridspec
+		fig1 = plt.figure(figsize=figsize)
+		fig2 = plt.figure(figsize=figsize)
+		fig3 = plt.figure(figsize=figsize)
+		gs1 = gridspec.GridSpec(num_rows, num_cols, figure=fig1, hspace=0, wspace=0)
+		gs2 = gridspec.GridSpec(num_rows, num_cols, figure=fig2, hspace=0, wspace=0)
+		gs3 = gridspec.GridSpec(num_rows, num_cols, figure=fig2, hspace=0, wspace=0)
+		for irow, alpha in enumerate(alphas):
+			for icol, beta in enumerate(betas):
+				print('Current working directory:', os.getcwd())
+				dname = 'alpha_%.2lf_beta_%.2lf_gamma_%.2lf'%(alpha, beta, gamma)
+				if not os.path.isdir(dname):
+					print('No directory %s found, creating...'%dname)
+					os.makedirs(dname)
+				os.chdir(dname)
+				
+				if os.path.isfile(homdir+'/nbody_path.txt'):
+					print('Found nbody_path, copying to current directory')
+					shutil.copy(homdir+'/nbody_path.txt', './nbody_path.txt')
+				else:
+					print('No nbody_path found, assuming default is correct...')
 
-                # Generate positions
-                rs = generate_positions(N, a)
 
-                # Generate velocities
-                vs = generate_velocities(N, rs*pc2cm, M*Msol2g, a*pc2cm, Mg0=Mgas*Msol2g, ag0=agas*pc2cm, G=Gcgs)
 
-                ms = np.ones(N) #assign_masses(rs, mmin=mmin)
-                
-                vs /= km2cm
+				Mgas = alpha*M
+				agas = beta*a
+				if not os.path.isfile('sim_ics_r.npy') or not os.path.isfile('sim_ics_v.npy') or not os.path.isfile('sim_ics_m.npy'):
 
-                np.save('sim_ics_r', rs)
-                np.save('sim_ics_v', vs)
-                np.save('sim_ics_m', ms)
-                np.save('sim_gparams', gparams)
-            
-            else:
-                rs = np.load('sim_ics_r.npy')
-                vs = np.load('sim_ics_v.npy')
-                ms = np.load('sim_ics_m.npy')
-                gparams=  np.load('sim_gparams.npy')
-            
-            nbins0= 0
-            
+					tcross = a*pc2cm/plummer_sigmav(0.0,M*Msol2g, a*pc2cm, G=Gcgs)/Myr2s
+					texp = tcross*gamma
+					print('Tcross:', tcross, 'Texp:', texp)
+					#gparams = define_gasjumps(Mgas, -Mgas/trem, njumps=1, tdelay=1.0, tend=tend, ascale=1.0)
+					if texp==0.0:
+						gparams = None #np.array([Mgas, agas,0.0, trem])
+					else:
+						gparams = np.array([Mgas, agas,Mgas/texp, trem])
 
-            sim = nbi.nbody6_cluster(rs.T, vs.T, ms,  outname='clustersim', dtsnap_Myr =10.0, \
-                        tend_Myr = tend, gasparams=gparams, etai=0.005, etar=0.005, etau=0.01, dtmin_Myr=1e-8, \
-                        rmin_pc=1e-5,dtjacc_Myr=1.0, load=True, ctype='smooth', force_incomp = False, \
-                        rtrunc=50.0, nbin0=nbins0, aclose_au=200.0)
-            #sim.store_arrays(reread=True)
-            sim.evolve(reread=True, suppress_restart=True)
-            #exit()
-            txt = f'$\\alpha = {alpha}$, $\\beta = {beta}$'
-            if os.path.isdir('pre_exp'):
-            	shutil.rmtree('pre_exp')
-            if os.path.isdir('post_exp'):
-            	shutil.rmtree('post_exp')
+					mmass = 1.
+					N = int(M/mmass)
 
-            if irow==0 and icol==0:
-                ax1 = fig1.add_subplot(gs1[irow, icol])
-                ax2 = fig2.add_subplot(gs2[irow, icol])
-                ax3 = fig3.add_subplot(gs3[irow, icol])
-            else:
-                ax1 = fig1.add_subplot(gs1[irow, icol], sharex=ax1, sharey=ax1)
-                ax2 = fig2.add_subplot(gs2[irow, icol], sharex=ax2, sharey=ax2)
-                ax3 = fig3.add_subplot(gs3[irow, icol], sharex=ax3, sharey=ax3)
-                
-            cp.plot_radii(sim, agas=gparams[1], Mgas=gparams[0], axtext=txt, axhmr=ax1, axrif=ax2, axErf=ax3)
+					# Generate positions
+					rs = generate_positions(N, a)
 
-            # Remove tick labels except for the left and bottom-most panels
-            # Adjust tick labels for the left-most column and bottom-most row
-            if icol == 0:
-                ax1.tick_params(axis='y', which='both', labelleft=True)
-                ax2.tick_params(axis='y', which='both', labelleft=True)
-                ax3.tick_params(axis='y', which='both', labelleft=True)
-            else:
-                ax1.tick_params(axis='y', which='both', labelleft=False)
-                ax2.tick_params(axis='y', which='both', labelleft=False)
-                ax3.tick_params(axis='y', which='both', labelleft=False)
-                ax1.set_ylabel('')
-                ax2.set_ylabel('')
-                ax3.set_ylabel('')
-                
-            if irow == num_rows - 1:
-                ax1.tick_params(axis='x', which='both', labelbottom=True)
-                ax2.tick_params(axis='x', which='both', labelbottom=True)
-                ax3.tick_params(axis='x', which='both', labelbottom=True)
-            else:
-                ax1.tick_params(axis='x', which='both', labelbottom=False)
-                ax2.tick_params(axis='x', which='both', labelbottom=False)
-                ax3.tick_params(axis='x', which='both', labelbottom=False)
-                ax1.set_xlabel('')
-                ax2.set_xlabel('')
-                ax3.set_xlabel('')
-           
-            os.chdir(homdir)
-            del sim
+					# Generate velocities
+					vs = generate_velocities(N, rs*pc2cm, M*Msol2g, a*pc2cm, Mg0=Mgas*Msol2g, ag0=agas*pc2cm, G=Gcgs)
 
-            fig1.savefig('hmr_all.pdf', bbox_inches='tight', format='pdf')
-            fig2.savefig('rinit_v_rfinal.pdf', bbox_inches='tight', format='pdf')
-            fig3.savefig('Einit_v_rfinal.pdf', bbox_inches='tight', format='pdf')
-            #if icol==1:
-            #    exit()
-            #plt.show()
+					ms = np.ones(N) #assign_masses(rs, mmin=mmin)
 
-    fig1.tight_layout(pad=0)
-    fig2.tight_layout(pad=0)
-    fig3.tight_layout(pad=0)
-    fig1.savefig('hmr_all.pdf', bbox_inches='tight', format='pdf')
-    fig2.savefig('rinit_v_rfinal.pdf', bbox_inches='tight', format='pdf')
-    fig3.savefig('Einit_v_rfinal.pdf', bbox_inches='tight', format='pdf')
-    plt.show()
+					vs /= km2cm
+
+					np.save('sim_ics_r', rs)
+					np.save('sim_ics_v', vs)
+					np.save('sim_ics_m', ms)
+					if not gparams is None:
+						np.save('sim_gparams', gparams)
+
+				else:
+					rs = np.load('sim_ics_r.npy')
+					vs = np.load('sim_ics_v.npy')
+					ms = np.load('sim_ics_m.npy')
+					if os.path.isfile('sim_gparams.npy'):
+						gparams=  np.load('sim_gparams.npy')
+					else:
+						gparams=None
+
+				nbins0= 0
+
+
+				sim = nbi.nbody6_cluster(rs.T, vs.T, ms,  outname='clustersim', dtsnap_Myr =10.0, \
+				tend_Myr = tend, gasparams=gparams, etai=0.05, etar=0.05, etau=0.05, dtmin_Myr=1e-5, \
+				rmin_pc=1e-5,dtjacc_Myr=1.0, load=True, ctype='smooth', force_incomp = False, \
+				rtrunc=50.0, nbin0=nbins0, aclose_au=200.0)
+				#sim.store_arrays(reread=True)
+				sim.evolve(reread=True, suppress_restart=True)
+				#exit()
+				txt = f'$\\alpha = {alpha}$, $\\beta = {beta}$'
+				if os.path.isdir('pre_exp'):
+					shutil.rmtree('pre_exp')
+				if os.path.isdir('post_exp'):
+					shutil.rmtree('post_exp')
+
+				if irow==0 and icol==0:
+					ax1 = fig1.add_subplot(gs1[irow, icol])
+					ax2 = fig2.add_subplot(gs2[irow, icol])
+					ax3 = fig3.add_subplot(gs3[irow, icol])
+				else:
+					ax1 = fig1.add_subplot(gs1[irow, icol], sharex=ax1, sharey=ax1)
+					ax2 = fig2.add_subplot(gs2[irow, icol], sharex=ax2, sharey=ax2)
+					ax3 = fig3.add_subplot(gs3[irow, icol], sharex=ax3, sharey=ax3)
+
+				cp.plot_radii(sim, agas=agas, Mgas=Mgas, axtext=txt, axhmr=ax1, axrif=ax2, axErf=ax3)
+
+				# Remove tick labels except for the left and bottom-most panels
+				# Adjust tick labels for the left-most column and bottom-most row
+				if icol == 0:
+					ax1.tick_params(axis='y', which='both', labelleft=True)
+					ax2.tick_params(axis='y', which='both', labelleft=True)
+					ax3.tick_params(axis='y', which='both', labelleft=True)
+				else:
+					ax1.tick_params(axis='y', which='both', labelleft=False)
+					ax2.tick_params(axis='y', which='both', labelleft=False)
+					ax3.tick_params(axis='y', which='both', labelleft=False)
+					ax1.set_ylabel('')
+					ax2.set_ylabel('')
+					ax3.set_ylabel('')
+
+				if irow == num_rows - 1:
+					ax1.tick_params(axis='x', which='both', labelbottom=True)
+					ax2.tick_params(axis='x', which='both', labelbottom=True)
+					ax3.tick_params(axis='x', which='both', labelbottom=True)
+				else:
+					ax1.tick_params(axis='x', which='both', labelbottom=False)
+					ax2.tick_params(axis='x', which='both', labelbottom=False)
+					ax3.tick_params(axis='x', which='both', labelbottom=False)
+					ax1.set_xlabel('')
+					ax2.set_xlabel('')
+					ax3.set_xlabel('')
+
+				os.chdir(homdir)
+				del sim
+
+				fig1.savefig('hmr_all_%d.pdf'%ig, bbox_inches='tight', format='pdf')
+				fig2.savefig('rinit_v_rfinal_%d.pdf'%ig, bbox_inches='tight', format='pdf')
+				fig3.savefig('Einit_v_rfinal_%d.pdf'%ig, bbox_inches='tight', format='pdf')
+				#if icol==1:
+				#    exit()
+				#plt.show()
+
+		fig1.tight_layout(pad=0)
+		fig2.tight_layout(pad=0)
+		fig3.tight_layout(pad=0)
+
+		fig1.savefig('hmr_all_%d.pdf'%ig, bbox_inches='tight', format='pdf')
+		fig2.savefig('rinit_v_rfinal_%d.pdf'%ig, bbox_inches='tight', format='pdf')
+		fig3.savefig('Einit_v_rfinal_%d.pdf'%ig, bbox_inches='tight', format='pdf')
+		plt.show()
